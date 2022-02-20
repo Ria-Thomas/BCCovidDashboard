2db2e49be9af7b1050ba4f976513e78aa3b417f5
Ria Thomas <riat06@gmail.com>
2022-02-20 14:26:05 -0800

    Update data source and README

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 ### BC Covid Dashboard
 
-This contains a basic Tableau Public Dashboard to show simple stats on Covid cases in BC, Canada. 
+This contains a basic Tableau Public Dashboard to show simple stats on Covid cases in BC, Canada.
 You can access the dashboard [here](https://public.tableau.com/app/profile/ria.thomas7951/viz/BCCovid19Dashboard/BCCovidDashboard)
 
 ![Dashboard snapshot](images/dash.png)
+
+#### About the data source
+
+The [dataset](http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv) is by [BC.COVID19.EDIT](http://www.bccdc.ca/health-info/diseases-conditions/covid-19/data).
+
+Check the licenses and data disclaimer [here](http://www.bccdc.ca/Health-Info-Site/Documents/BC_COVID-19_Disclaimer_Data_Notes.pdf) 
--- get_data.py
+++ get_data.py
@@ -9,16 +9,22 @@ import datetime
 import sqlalchemy as db
 import keyring
 import pymysql
+import urllib.request
 
 def load_table():
 
     engine_dest = db.create_engine('mysql+pymysql://root:'+keyring.get_password('mysql_db', 'root')+'@localhost/db_dash?host=localhost?port=3306')
     table_name = 'bc_covid_data'
+    
     print('Getting data')
-    df = pd.read_csv('./data/B.C._COVID-19_-_Case_Details.csv')
+    source_file_name = 'http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv'
+    urllib.request.urlretrieve(source_file_name, "./data/BCCDC_COVID19_Dashboard_Case_Details.csv")
+    df = pd.read_csv('./data/BCCDC_COVID19_Dashboard_Case_Details.csv')
 
     print('Got the data! Now cleaning..')
     df['Reported_Date'] = pd.to_datetime(df['Reported_Date']).dt.strftime('%Y/%m/%d')
+    df['ObjectId'] = range(1, len(df.index)+1)
+    df.drop('Classification_Reported', axis=1, inplace=True)
 
     print('All done! Getting ready to load it to the table')
     conn = engine_dest.connect()
